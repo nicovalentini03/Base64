39cd2efc90b1ba2139e9dec965c1c2e666a80375
nicolo' valentini <nicolo.valentini03@outlook.it>
2019-11-23 15:24:55 +0100

    Aggiungere i file di progetto.

---
+++ LibraryBase64/Base64.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBase64
+{
+    public class Base64
+    {
+            public static string EsBase64()
+            {
+                string toEncode = "nicolo";
+                byte[] toEncodeAsBytes = ASCIIEncoding.ASCII.GetBytes(toEncode);
+
+                string returnValue = Convert.ToBase64String(toEncodeAsBytes);
+
+                return returnValue;
+            }
+    }
+
+
+}
---
+++ TestBase64/ToBase64Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LibraryBase64;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBase64
+{
+    [TestClass]
+    public class ToBase64Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string risposta = Base64.EsBase64();
+            Assert.AreEqual("cgfidd34i", risposta);
+        }
+    }
+}
